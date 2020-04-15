16a0b4e417e3478b1bc4f83864aae9fdc8897c93
Markian Hromiak <mhromiak3@gatech.edu>
2020-04-15 00:11:26 -0400

    added api to production envorinment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgG8sKDaJesw1DADnXGDX6XPkG9bQLmd4',
+    authDomain: 'driving-force-3e9f1.firebaseapp.com',
+    databaseURL: 'driving-force-3e9f1.firebaseio.com',
+    projectId: 'driving-force-3e9f1',
+    storageBucket: 'driving-force-3e9f1.appspot.com',
+    messagingSenderId: '368863809536'
+  }
 };
