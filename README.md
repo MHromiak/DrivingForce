274c9174e1826c5560853ccedf55b88301a9eab4
Markian Hromiak <mhromiak3@gatech.edu>
2020-10-24 23:43:47 -0400

    Hostable version of the app. Need to update database

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Spring3300pr2
+# DrivingForce
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spring3300pr2'`, () => {
+  it(`should have as title 'DrivingForce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spring3300pr2');
+    expect(app.title).toEqual('DrivingForce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spring3300pr2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('DrivingForce app is running!');
   });
 });
